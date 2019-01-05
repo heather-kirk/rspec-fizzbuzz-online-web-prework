e8f7c5c0816dcfe1b8fd94ebc7658d8abd91ecf7
Heather Kirk <hskirk@gmail.com>
2019-01-05 02:50:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,12 @@
 
 
 def fizzbuzz(int)
-  if int % 3 == 0 
-    "Fizz"
-  elsif int % 5 == 0 
-  "Buzz"
-elsif int % 15 == 3
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0 
+  "Fizz"
 "FizzBuzz"
-elsif int % 3 == 0 && int % 5 == 0
-"FizzBuzz"
+elsif int % 5 == 0
+"Buzz"
 end
 end
